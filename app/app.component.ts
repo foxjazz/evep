9aa16f24ec591b78b7ee13c471293833223f0ebf
Joe <foxr4@foxjazz.net>
2016-05-11 22:44:15 -0600

    more updates

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,22 +3,25 @@ import {Region} from './region';
 import {EveRegionListComponent} from './everegion-list.component';
 import { HTTPEveService } from './http-eve.service';
 import 'rxjs/Observable';
-import {Regions} from './mock-Regions'
+import {Regions} from './mock-Regions';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>',
+    template: `<h1>My First Angular 2 App</h1>
+    <div class="error" *ngIf="errorMessage">{{errorMessage}}</div>
+    `,
     providers: [HTTPEveService]
 })
 export class AppComponent implements OnInit {
     private title ="Regions List";
+    private errorMessage = '';
     private regions: Region[];
       constructor(private eveService: HTTPEveService) { }
      
      getRegions(){
          this.eveService.getRegions()
                 .subscribe(Regions => this.regions = Regions,
-                     error =>  console.log('cannot load regions'));
+                    error =>  this.errorMessage = <any>error);
      }
      ngOnInit() {
     this.getRegions();
