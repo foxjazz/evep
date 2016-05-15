69a1e33ec2a00ef1b12c50df83f0b740cc6670d3
Joe <foxr4@foxjazz.net>
2016-05-14 22:38:43 -0600

    added a working dealy

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,11 +16,11 @@ import {Regions} from './mock-Regions';
     <h2> function counter = {{cntr}}</h2>
     <div class="error" *ngIf="errorMessage">{{errorMessage}}</div>
     <ul>
-    <li *ngFor="let Region of Regs">
+    <li *ngFor="let Region of eveService.Regions">
       {{ Region.name }}
       </li>
   </ul>
-  <h2> hasloaded = {{loaded}} </h2>
+  <h2> is loading = {{eveService.loading}} </h2>
     `,
     providers: [HTTPEveService, HTTP_PROVIDERS]
 })
